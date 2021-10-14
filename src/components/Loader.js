302827ae34a837f8657444b439713b33e0fa072c
natlines1492 <natlines14@gmail.com>
2021-10-13 23:36:22 -0500

    Loader component ready!

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,24 @@
-
+import data from "../data/data.json"
+import { useState, useEffect} from "react";
 /*
  * Use useEffect to load and show the data results from data.json
  * this action will happen just when the component is mounted.
  */
 export const Loader = () => {
-  
+
+  console.log(data);
+  const [dataJson, setDataJson] = useState([]);
+
+  useEffect(() => {
+    setDataJson(data.results);
+  }, [])
+
   return (
-    <p>Loader</p>
+    <>
+      <p>Loader</p>
+      {dataJson.map(result => 
+        <p>{result.name}</p>
+        )}
+    </>
   );
 };
